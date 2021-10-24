02027eb256c94a89f99f5208b4e7dbd7ac9dfecc
allachetan <43319083+allachetan@users.noreply.github.com>
2021-10-23 20:17:45 -0400

    Update audio_features.py

--- data_processing/audio_features.py
+++ data_processing/audio_features.py
@@ -10,30 +10,21 @@ import math
 import json
 import tensorflow_hub as hub
 
-def extract_sentences(file, script_dir = "../data/GENEA/source/script/"):
+
+def extract_sentences(file, script_dir="../data/GENEA/source/script/"):
     f = open(script_dir + file + ".json")
     script = json.load(f)
     f.close()
-    script = script[0]["alternatives"][0]["words"]
+    script = script["results"]
 
     sentences = []
 
-    sentence = ""
-    start = -1
-    for word in script:
-        if word["word"][-1] == ".":
-            sentence += word["word"]
-            sentences.append({
-                "start_time": start,
-                "end_time": word["end_time"],
-                "sentence": sentence
-            })
-            sentence = ""
-            start = -1
-        else:
-            if start == -1:
-                start = word["start_time"]
-            sentence += word["word"] + " "
+    for sentence in script:
+        sentences.append({
+            "start_time:": sentence["alternatives"][0]["words"][0]["startTime"],
+            "end_time": sentence["alternatives"][0]["words"][-1]["endTime"],
+            "sentence": sentence["alternatives"][0]["transcript"]
+        })
 
     embed = hub.load("https://tfhub.dev/google/universal-sentence-encoder/4")
 
@@ -49,41 +40,43 @@ def extract_sentences(file, script_dir = "../data/GENEA/source/script/"):
 
     return sentences
 
+
 def extract_melspec(audio_dir, files, destpath, fps, sentence_path):
     for f in range(len(files)):
         sentences = extract_sentences(sentence_path[f])
         file = os.path.join(audio_dir, files[f] + '.wav')
         outfile = destpath + '/' + files[f] + '.npy'
-        
-        print('{}\t->\t{}'.format(file,outfile))
-        fs,X = wav.read(file)
-        X = X.astype(float)/math.pow(2,15)
 
-        hop_len=int(fs/fps)
+        print('{}\t->\t{}'.format(file, outfile))
+        fs, X = wav.read(file)
+        X = X.astype(float) / math.pow(2, 15)
+
+        hop_len = int(fs / fps)
 
         melspec_feat = np.empty((539, 0))
         start = 0
         for i in range(len(sentences)):
             end = int(float(sentences[i]["end_time"][:-1]) * fs) if i < len(sentences) - 1 else len(X)
 
-            features = librosa.feature.melspectrogram(y=X[start:end], sr=fs, n_fft=2048, hop_length=hop_len, n_mels=27, fmin=0.0, fmax=8000)
+            features = librosa.feature.melspectrogram(y=X[start:end], sr=fs, n_fft=2048, hop_length=hop_len, n_mels=27,
+                                                      fmin=0.0, fmax=8000)
             features = np.log(features)
             embeddings = np.tile(sentences[i]["embeddings"], (features.shape[1], 1))
             embeddings = np.transpose(embeddings)
             features = np.append(features, embeddings, axis=0)
 
-            melspec_feat = np.append(melspec_feat, features, axis = 1)
+            melspec_feat = np.append(melspec_feat, features, axis=1)
 
-        np.save(outfile,np.transpose(melspec_feat))
+        np.save(outfile, np.transpose(melspec_feat))
+
 
 def get_dfd(mfcc_features):
     mfcc_features_1d = mfcc_features[2:] - mfcc_features[1:-1]
     mfcc_features_2d = mfcc_features_1d[1:] - mfcc_features_1d[:-1]
     return np.concatenate((mfcc_features, mfcc_features_1d, mfcc_features_2d), axis=0)
 
+
 def extract_mfcc(audio_dir, files, destpath, sentence_path):
-
-
     for f in range(len(files)):
         sentences = extract_sentences(sentence_path[f])
         file = os.path.join(audio_dir, files[f] + ".wav")
@@ -92,7 +85,7 @@ def extract_mfcc(audio_dir, files, destpath, sentence_path):
         print('{}\t->\t{}'.format(file, outfile))
         X, sr = librosa.load(file)
 
-        mfcc_features = np.empty((549,0))
+        mfcc_features = np.empty((549, 0))
         start = 0
         for i in range(len(sentences)):
             end = int(float(sentences[i]["end_time"][:-1]) * sr) if i < len(sentences) - 1 else len(X)
@@ -107,11 +100,3 @@ def extract_mfcc(audio_dir, files, destpath, sentence_path):
             start = end
 
         np.save(outfile, np.transpose(mfcc_features))
-
-
-
-
-
-
-
-
